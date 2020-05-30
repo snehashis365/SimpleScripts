737d45e89648c73e0dcbb764a910ae9ef93d4b0f
Snehashis Sarkar <36337263+snehashis365@users.noreply.github.com>
2020-05-30 16:10:32 +0530

    Options and option argument handling added
    Following options added
    -a :-Turn on alarm on no reply | Alarm is off by default now
    -t <DELAY> :- Set ping deadline to <DELAY> hence changing the interval between status message update 
    
    Minor Cleanups here and there added a few comment lines

--- pingChecker.sh
+++ pingChecker.sh
@@ -1,42 +1,76 @@
 #!/bin/bash
+#Declaring required variables
+LGREEN='\033[1;32m'
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+NORMAL='\033[0m'
+BEEP='' #Alarm off by default
+DEFAULT_IP_FLAG=true
+IP='8.8.8.8'
+DELAY='1'
 #Function to handle Ctrl+C
 function endScript ()
 {
 	#Handle Clean up here | Plans are to show a summary at script exit
-	echo -e "\n********************************************************"
+	echo -e "\n****************************************************"
 	exit 2
 }
 #Setting trap to call endScript function with SIGINT(2)
 trap "endScript" 2
 #This script will check ping with provided IP/domain/Default Google DNS and show the connection status
-#Updated every 1s by default argument handling coming soon
-LGREEN='\033[1;32m'
-RED='\033[0;31m'
-BLUE='\033[0;34m'
-NORMAL='\033[0m'
-BEEP='\007'
-FLAG=true
-IP='8.8.8.8'
-echo -e "*******${LGREEN}Connection Status ${BLUE}notifier${NORMAL} by ${LGREEN}Snehashis${NORMAL}**********"
+#Updated every 1s by default| Options and option argument handling added still a prorotype
+
+#Options and options arguments handling:-
+while getopts ":at:" opt; do
+	case ${opt} in
+		a ) #Turn on the alarm on no reply
+			BEEP='\007'
+			;;
+		t ) #Manually set delay
+			if [[ "$OPTARG" == *"."* ]]; then
+				echo -e "${RED}Invalid option:${NORMAL} '$OPTARG' is not an INTEGER" 1>&2
+				exit 2
+			else
+				DELAY=$OPTARG
+			fi
+			;;
+		\? )
+			echo "Invalid option: $OPTARG" 1>&2
+			exit 2
+			;;
+		: ) 
+			echo -e "${RED}Invalid option:${NORMAL} -$OPTARG requires an argument <INTEGER>"
+			exit 2
+      		;;
+	esac
+done
+shift $((OPTIND -1))
+
+
+echo -e "******${LGREEN}Connection Status ${BLUE}notifier${NORMAL} by ${LGREEN}Snehashis${NORMAL}*******"
+if [ "$DELAY" -ne "1" ]
+then
+	echo -e "\n${LGREEN}Delay ${BLUE}manually set${NORMAL} to ${RED}${DELAY}"
+fi
 if [ $# -gt 0 ]
 then
 	if [ "$#" == 1 ]
 	then
 		IP=$1
-		FLAG=false
+		DEFAULT_IP_FLAG=false
 	else
 		#Argument handling here | Plans to make deadline and alert beep on or off as user settable
 		echo -n
 	fi
 fi
-if [ "$FLAG" = true ]
+if [ "$DEFAULT_IP_FLAG" = true ]
 then
 	echo -e "${LGREEN}Checking default Google DNS${NORMAL}"
 fi	
 echo -e "${BLUE}Pinging ${NORMAL}.....${LGREEN} ${IP}${NORMAL}\n"
-while((1))
+while((1)) #The scipt rus infinitely unless control break(Ctrl+c) occurs
 do
-	OUTPUT=$(ping -w 1 ${IP} | grep "Destination Host Unreachable\|100% packet loss")
+	OUTPUT=$(ping -w ${DELAY} ${IP} | grep "Destination Host Unreachable\|100% packet loss")
 	if test -z "$OUTPUT"
 	then
 		echo -ne "\r${LGREEN}Connection OK${NORMAL} "
