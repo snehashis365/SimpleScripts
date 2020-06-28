341a3f3d0cdc302d137573ac5ca1d160359595b0
snehashis365 <snehashis.2000@gmail.com>
2020-06-29 03:32:18 +0530

    Added Core Functions (Script Usable Now) Changelog:- Added sub menu builder Added run and compile functions Added script termination function Update menu builder Re-arranged code for better readability Update help message Changed Banner(So called) Added trap for control break Added Press escape to continue on compilation/execution Fixed syntax errors on boolean checks
    Coming Soon:
    Install Script to local bin for easy usage in any directory

--- cRun.sh
+++ cRun.sh
@@ -9,29 +9,10 @@ BEEP='\007'
 MAKE_MENU=false
 COMPILE=true
 RUN=true
+DEL_OBJ=false
 SHOW_TIME=false
-function buildMenu ()
-{
-	INDEX=1
-	declare -a ITEM
-	ITEM[0]="Exiting"
-	while(($#))
-	do
-		echo -en "${INDEX}.   ${LGREEN}$1 ${NORMAL}"
-		ITEM[$((INDEX++))]=$1
-		echo
-		shift
-	done
-	echo
-	echo -e "0. ${LGREEN}Exit ${NORMAL}"
-	read -p "Select : " CHOICE
-	if [ $CHOICE -gt 0 -a $CHOICE -le $INDEX ]
-	then
-		clear
-		echo -e "${BLUE}Selected ->${LGREEN} ${ITEM[$CHOICE]} ${NORMAL}"
-	fi
 
-}
+clear
 
 #Function to convert and show seconds to more understandable time format
 function showTime () 
@@ -59,24 +40,166 @@ function showTime ()
     fi
 }
 
+function endScript ()
+{
+	#Handle Clean up here
+	echo -e "\nExiting....."
+	if [ $SHOW_TIME = true ]
+	then
+		showTime $SECONDS
+	fi
+	echo -e "***************************************************"
+	exit 2
+}
+
+function compile ()
+{
+	ERROR_COUNT=0
+	while(($#))
+	do
+		if [ -f ${1/.c*/.out} ]
+		then
+			echo -e "${BLUE}Re-Compiling${NORMAL}.....$1"
+		else	
+			echo -e "${BLUE}Compiling${NORMAL}.....$1"
+		fi
+		if [ $? -ne 0 ]
+		then
+			(($ERROR_COUNT++))
+		fi
+		cc $1 -o ${1/.c*/.out} -lm
+		echo -e "${LGREEN}Done ${NORMAL}"
+		shift
+	done
+	#echo -e "Processed ${LGREEN} $count ${NORMAL}files."
+	echo -e "${LGREEN}Object files generated${NORMAL}"
+	if [ $ERROR_COUNT -gt 0 ]
+	then
+		echo -e "${RED}${ERROR_COUNT} Errors${NORMAL} were encountered by ${BLUE}GCC compiler${LGREEN} ignoring warnings${NORMAL}."
+		return $ERROR_COUNT
+	fi
+	if [ "$RUN" = false ]
+	then
+		echo -e "Compile Output will be cleared Press ctrl+c to exit the script and keep output otherwise\n"
+		read -r -s -p $'Press escape to continue...\n' -d $'\e'
+	fi
+}
+
+function run ()
+{
+	COMPILE_ERROR=false
+	while(($#))
+	do
+		if [ -f ${1/.c*/.out} -o "$COMPILE" = true ]
+		then
+			compile $1
+			if [ $? -ne 0 ]
+			then
+				COMPILE_ERROR=true
+			else
+				COMPILE_ERROR=false
+			fi
+		fi
+		if [ COMPILE_ERROR=false ]
+		then
+			echo -e "${BLUE}Executing${NORMAL}.....$1"
+			./${1/.c*/.out}
+			if [ "$DEL_OBJ" = true ]
+			then
+				echo -e "${RED}Removing Object File${NORMAL}......$1"
+				rm ${1/.c*/.out}
+			fi
+			echo -e "${LGREEN}Done ${NORMAL}"
+		else
+			echo -e "${RED}Cannot Run${NORMAL}.....$1"
+		fi
+		shift
+	done
+	echo -e "Output will be cleared Press ctrl+c to exit the script and keep output otherwise\n"
+	read -r -s -p $'Press escape to continue...\n' -d $'\e'
+}
+
+function buildSubMenu ()
+{
+	while((1))
+	do
+		clear
+		echo -e "\n*************${LGREEN}c${BLUE}Run${NORMAL} by ${LGREEN}snehashis365${NORMAL}***************\n\n"
+		echo -e "${BLUE}Selected ->${LGREEN} $1 ${NORMAL}"
+		echo -e "1. ${LGREEN}Run ${NORMAL}(Automatically compile if object file missing)"
+		echo -e "2. ${BLUE}Compile Only ${NORMAL}"
+		echo -e  "\n9. Return to Main Menu"
+		read -p "Select : " CHOICE
+		if((CHOICE==1));then
+			run $1
+		elif((CHOICE==2));then
+			RUN=false
+			compile $1
+			RUN=true
+		elif((CHOICE==9));then
+			return 2
+		else
+			clear
+			echo -e "\n*************${LGREEN}c${BLUE}Run${NORMAL} by ${LGREEN}snehashis365${NORMAL}***************\n\n"
+			echo -e "${RED}Invalid Option${NORMAL}\nPlease Try Again"
+			echo -e "${BLUE}Selected ->${LGREEN} $1 ${NORMAL}"
+			buildSubMenu $1
+		fi
+	done
+}
+
+function buildMenu ()
+{
+	INDEX=1
+	declare -a ITEM
+	ITEM[0]="Item_List" #Occuying 1st index just cause I want to don't question this it's unecessary
+	while(($#))
+	do
+		COMPILE_STATUS='\033[0;31m'
+		if [ -f ${1/.c*/.out} ]
+		then
+			COMPILE_STATUS='\033[1;32m'
+		fi
+		echo -en "${INDEX}. ${COMPILE_STATUS}$1 ${NORMAL}"
+		ITEM[$((INDEX++))]=$1
+		echo
+		shift
+	done
+	echo
+	echo -e "0. ${LGREEN}Exit ${NORMAL}"
+	read -p "Select : " CHOICE
+	if [ $CHOICE -gt 0 -a $CHOICE -le $INDEX ]
+	then
+		buildSubMenu ${ITEM[$CHOICE]}
+	elif [ $CHOICE -eq 0 ]
+	then
+		endScript
+	fi
+}
+
 #Function to show help message
 function help ()
 {
 	echo "This script will compile the files specified and generator object files with same name as the C file and Execute them in the other named."
-   echo
-   echo "Syntax: ./cRun.sh [-h|c|r|m|t|i]"
-   echo "options:"
-   echo "h     Print this Help."
-   echo "c	   Compile/Re-compile"
-   echo "r     Run relevant object file without recompiling"
-   #echo "v     Verbose mode."
-   echo "m     Build a menu with the provided files"
-   echo "t     Show total time taken to execute the script"
-   echo "i     Install the script to /usr/local/bin to ease"
-   echo
+	echo
+	echo "Syntax: ./cRun.sh [-h|c|r|m|t|d|i]"
+	echo "options:"
+	echo "h     Print this Help."
+	echo "c	    Compile only/Re-compile(If object file present)"
+	echo "r     Run relevant object file without recompiling(Automatically compile if object file missing)"
+	#echo "v     Verbose mode."
+	echo "m     Build a menu with the provided files"
+	echo "t     Show total time taken to execute the script"
+	echo "d     Delete Object file after it has been execued"
+	echo "i     Install the script to /usr/local/bin to ease"
+	echo
 }
+
+#Setting trap to call endScript function with SIGINT(2)
+trap "endScript" 2
+
 #Options and options arguments handling:-
-while getopts ":hcrmti" opt; do
+while getopts ":hcrmtdi" opt; do
 	case ${opt} in
 		h ) #Display Help message
 			help
@@ -86,7 +209,13 @@ while getopts ":hcrmti" opt; do
 			COMPILE=true
 			RUN=false
 			;;
-		r ) #Run without re-compiling
+		r ) #Run without re-compiling 
+			if [ "$RUN" = false ]
+			then
+				echo -e "${BLUE}No need to specify both -r and -c${NORMAL}(They are enabled by default prefer these 2 are prefered to be used exclusively)"
+			else
+				COMPILE=false #To avoid re-compilaion
+			fi
 			RUN=true
 			;;
 		m )
@@ -95,8 +224,12 @@ while getopts ":hcrmti" opt; do
 		t ) #Show shell execution duration
 			SHOW_TIME=true
 			;;
+		d ) #Delete Object file after running the program
+			echo -e "Object Files will be ${RED}DELETED${BLUE} After Execution${NORMAL}"
+			DEL_OBJ=true
+			;;
 		i ) #Install
-			echo -e "${RED}For LINUX/WSL Systems only\n${BLUE}Coming Soon..."
+			echo -e "${RED}For LINUX System/WSL Environment only\n${BLUE}Coming Soon..."
 			exit 2
 			;;
 		\? )
@@ -107,27 +240,37 @@ while getopts ":hcrmti" opt; do
 done
 shift $((OPTIND -1))
 
-clear
-echo -e "${LGREEN}Multiple ${BLUE}C${NORMAL} files compiler by ${LGREEN}Snehashis${NORMAL}."
+echo -e "\n*************${LGREEN}c${BLUE}Run${NORMAL} by ${LGREEN}snehashis365${NORMAL}***************\n\n"
 if [ $# -gt 0 ]
 then
 	count=$#
 	while(($#))
 	do
-		echo -e "${BLUE}Compiling${NORMAL}.....$1"
-
-		DOT_C=".c"
-		cc $1 -o ${1/$DOT_C*/.out} -lm
-		echo -e "${LGREEN}Done ${NORMAL}"
+		if [ "$MAKE_MENU" = true ]
+		then
+			while((1))
+			do
+				buildMenu "$@"
+				clear
+				echo -e "\n*************${LGREEN}c${BLUE}Run${NORMAL} by ${LGREEN}snehashis365${NORMAL}***************\n\n"
+			done
+		elif [ "$RUN" = true ]
+		then
+			run "$@"
+		elif [ "$COMPILE" = true ]
+		then
+			compile "$@"
+		fi
 		shift
 	done
 	echo -e "Processed ${LGREEN} $count ${NORMAL}files."
 	echo -e "${LGREEN}Object files should be generated${NORMAL} if no ${RED}errors${NORMAL} were encountered by ${BLUE}GCC compiler${LGREEN} ignore warnings${NORMAL}."
 else
 	echo -e "${RED}Experimental!!!\n${NORMAL}Building Menu...\n"
-	buildMenu *.c
-fi
-if [ $SHOW_TIME = true ]
-then
-	showTime $SECONDS
+	while((1))
+	do
+		buildMenu *.c
+		clear
+		echo -e "\n*************${LGREEN}c${BLUE}Run${NORMAL} by ${LGREEN}snehashis365${NORMAL}***************\n\n"
+	done
 fi
