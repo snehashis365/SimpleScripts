a0537414e9ce98bd1dc05217abc310db68e5ab3e
snehashis365 <snehashis.2000@gmail.com>
2020-07-18 15:52:13 +0000

    Hotfix for install status check

--- cRun.sh
+++ cRun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION='0.7.18.10'
+VERSION='0.7.18.11'
 #This script will compile the files specified and generator object files with same name as the C file and Execute them in the other named.
 #For e.g:- example.c will give example.out and execute example.out
 LGREEN='\033[1;32m'
@@ -51,14 +51,6 @@ else
     case $yn in
       [Yy]*)
         $PACKAGE_INSTALL
-        command -v cc >/dev/null 2>&1
-        if [ "$?" == "0" ] ; then
-          echo -e "${LGREEN}Install Success...\n${NORMAL}"
-          read -r -s -p $'Press escape to continue...\n' -d $'\e'
-        else
-          echo -e "${RED}Install failed....\n${NORMAL}"
-          exit 2
-        fi
         break
         ;;
       [Nn]*)
